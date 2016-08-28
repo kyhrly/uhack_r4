c1b25141b36d3979c032146de892b9ac12183988
kyhrly <kyhrly@gmail.com>
2016-08-28 12:59:41 +0800

    Add files via upload

--- hackathon_db.sql
+++ hackathon_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 28, 2016 at 02:09 AM
+-- Generation Time: Aug 28, 2016 at 06:30 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.20
 
@@ -62,7 +62,9 @@ CREATE TABLE `tblbidding` (
 
 INSERT INTO `tblbidding` (`bidID`, `postID`, `lawyerID`, `bidMessage`, `bidTime`) VALUES
 (1, '11', '5', 'I could help you for a price of 2,00php', '2016-08-27 23:01:24'),
-(2, '11', '5', 'If you want, you could just pm me', '2016-08-27 23:01:35');
+(2, '11', '5', 'If you want, you could just pm me', '2016-08-27 23:01:35'),
+(3, '11', '5', 'Hey PM me', '2016-08-28 02:20:44'),
+(4, '11', '5', '1000', '2016-08-28 03:17:24');
 
 -- --------------------------------------------------------
 
@@ -91,8 +93,6 @@ INSERT INTO `tblconversation` (`convoID`, `sender`, `userID`, `lawyerID`, `messa
 (16, 'client', '3', '5', 'heheheh', '2016-08-28 04:32:00'),
 (17, 'client', '3', '5', 'hi', '2016-08-28 04:32:07'),
 (18, 'client', '3', '5', 'hehehe', '2016-08-28 04:32:10'),
-(19, 'lawyer', '5', '5', 'test', '2016-08-28 04:56:40'),
-(22, 'lawyer', '', '5', 'test', '2016-08-28 05:06:14'),
 (23, 'lawyer', '3', '5', '2', '2016-08-28 05:11:00'),
 (24, 'client', '3', '5', 'test', '2016-08-28 05:12:28'),
 (25, 'lawyer', '3', '5', 'ow', '2016-08-28 05:12:31'),
@@ -105,7 +105,8 @@ INSERT INTO `tblconversation` (`convoID`, `sender`, `userID`, `lawyerID`, `messa
 (32, 'lawyer', '3', '5', 'I can help you', '2016-08-28 07:52:57'),
 (33, 'lawyer', '3', '5', 'but i need 1k', '2016-08-28 07:53:08'),
 (34, 'lawyer', '3', '5', 'hey', '2016-08-28 08:09:11'),
-(35, 'lawyer', '3', '5', 'answer please', '2016-08-28 08:09:15');
+(35, 'lawyer', '3', '5', 'answer please', '2016-08-28 08:09:15'),
+(36, 'client', '3', '5', 'Hey help me here please', '2016-08-28 10:19:59');
 
 -- --------------------------------------------------------
 
@@ -133,7 +134,8 @@ CREATE TABLE `tbllawyer` (
 --
 
 INSERT INTO `tbllawyer` (`lawyerID`, `lawyerUsername`, `lawyerPassword`, `lawyerLastName`, `lawyerFirstName`, `lawyerMiddleName`, `firmAssociatedTo`, `firmAddress`, `specialization`, `locationLawyers`, `casesWon`, `status`) VALUES
-(5, 'lawyer', 'sample', 'Santiago', 'Mirriam', 'Defensor', 'Senators'' Law Firm', 'Region 1, PH', 'Administrative', 'Region 1', '20', '1');
+(5, 'lawyer', 'sample', 'Santiago', 'Mirriam', 'Defensor', 'Senators'' Law Firm', 'Region 1, PH', 'Administrative', 'Region 1', '25', '1'),
+(6, 'Tagapagtanggol01', '12345', 'Tagapagtanggol', 'Yow', 'B', 'Strong and Firm Company', '123 Makati City', 'Federal Law', 'NCR', '5', '1');
 
 -- --------------------------------------------------------
 
@@ -156,7 +158,8 @@ CREATE TABLE `tblpost` (
 INSERT INTO `tblpost` (`postID`, `userID`, `postDescription`, `postTimeStamp`, `postSpecialization`) VALUES
 (11, '3', 'I killed someone but it is only to defend my self, I want someone who specializes in crime to help me here.', '2016-08-28 00:46:23', 'Administrative'),
 (12, '3', 'Test case 1', '2016-08-28 00:46:28', 'Administrative'),
-(13, '3', 'Test case 2', '2016-08-28 00:46:33', 'Administrative');
+(13, '3', 'Test case 2', '2016-08-28 00:46:33', 'Administrative'),
+(14, '5', 'My wife was accused of Drug Trafficking, She was framed up and is innocent, yet she is facing death penalty, I need help', '2016-08-28 11:11:04', 'Federal Law');
 
 -- --------------------------------------------------------
 
@@ -203,7 +206,9 @@ CREATE TABLE `tblusers` (
 --
 
 INSERT INTO `tblusers` (`userID`, `userUsername`, `userPassword`, `lastName`, `firstName`, `middleName`, `nickName`, `locationUsers`, `status`) VALUES
-(3, 'client', 'sample', 'Gaisler', 'Baron', 'Secret', '4', '5', '1');
+(3, 'client', 'sample', 'Gaisler', 'Baron', 'Secret', '4', '5', '1'),
+(4, 'JuanDelaCruz', '12345', 'Dela Cruz', 'Juan', '', 'JC', 'Makati City', '1'),
+(5, 'JuanDelaCruz', '12345', 'Dela Cruz', 'Juan', '', 'JC', 'Makati City', '1');
 
 --
 -- Indexes for dumped tables
@@ -264,22 +269,22 @@ ALTER TABLE `tbladmin`
 -- AUTO_INCREMENT for table `tblbidding`
 --
 ALTER TABLE `tblbidding`
-  MODIFY `bidID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `bidID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `tblconversation`
 --
 ALTER TABLE `tblconversation`
-  MODIFY `convoID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `convoID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 --
 -- AUTO_INCREMENT for table `tbllawyer`
 --
 ALTER TABLE `tbllawyer`
-  MODIFY `lawyerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `lawyerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `tblpost`
 --
 ALTER TABLE `tblpost`
-  MODIFY `postID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `postID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `tblpublish`
 --
@@ -289,7 +294,7 @@ ALTER TABLE `tblpublish`
 -- AUTO_INCREMENT for table `tblusers`
 --
 ALTER TABLE `tblusers`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
